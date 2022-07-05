9a956f3f2bafea1f56767c26ac4ddc3e897ba5ad
LizaGreenz <gunenkoliza@gmail.com>
2022-07-05 19:28:00 +0300

    Added button's state

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,13 +1,37 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  state = {
+    count: 0,
+  };
+
+  styles = {
+    fontWeight: "bold",
+    fontSize: 20,
+  };
+
   render() {
     return (
       <React.Fragment>
-        <h1>Hello world!</h1>
-        <button>Increment</button>
+        <span style={{ fontSize: 20 }} className={this.getBadgeClass()}>
+          {this.formatCount()}
+        </span>
+        <button style={this.styles} className="btn btn-secondary btn-sm">
+          Increment
+        </button>
       </React.Fragment>
     );
+  }
+
+  getBadgeClass() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
 
